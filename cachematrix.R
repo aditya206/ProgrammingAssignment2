a386ba0c01e5f19fa6617c57f6a0e5f3fc217983
aditya206 <35286320+aditya206@users.noreply.github.com>
2018-01-19 15:19:08 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The functions are designed to show the principle of closure i.e. writing a function within a function
+##These functions created below save computational time by creating special matrix that can cache 
+##its inverse and need not recalculate it if the matrix has not changed
 
-## Write a short comment describing this function
+##This first function, makeCacheMatrix creates a "special matrix" object that can cache its inverse
+##It returns list containing functions that help get the inverse of the special matrix 
+##This list then becomes the input to cacheSolve()
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()){
+  inver <- NULL
+  set <- function(y){
+    x <<- y
+    inver <<- NULL
+  }
+  get <- function() x
+  setInverse<-function(inverse) inver<<-inverse
+  getInverse<-function() inver
+  list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
+##This function calculates the inverse of original matrix that was an input to makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inver <- x$getInverse()
+  if(!is.null(inver)) {
+    message("getting cached data")
+    return(inver)
+  }
+  mat <- x$get()
+  inver <- solve(mat, ...)
+  x$setInverse(inver)
+  inver
 }
